889349f2369b18d15e42050337b2a14f8ee43bf6
till <till@php.net>
2012-10-23 17:22:36 +0200

    WIP deploy recipe for packagist. Update include for configuration.

--- packagist/recipes/default.rb
+++ packagist/recipes/default.rb
@@ -3,6 +3,11 @@ include_recipe "packagist::vhost"
 package "curl"
 package "git-core"
 
-include_recipe "packagist::vagrant"
+if node.attribute?("live")
+  include_recipe "packagist::deploy"
+end
+
+include_recipe "packagist::configure"
+
 include_recipe "packagist::db"
 include_recipe "packagist::cron"

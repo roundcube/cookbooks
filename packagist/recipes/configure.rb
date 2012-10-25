dcc7bea92070db89fb3ecf6ea1b09adc4ab31e48
till <till@php.net>
2012-10-25 02:43:31 +0200

    Refactored this.

--- packagist/recipes/configure.rb
+++ packagist/recipes/configure.rb
@@ -13,15 +13,3 @@ template "#{node["packagist"]["root"]}/app/config/parameters.yml" do
     :smtp          => node["packagist"]["smtp"]
   })
 end
-
-execute "curl -s https://getcomposer.org/installer | php" do
-  cwd node["packagist"]["root"]
-  not_if do
-    File.exists?("#{node["packagist"]["root"]}/composer.phar")
-  end
-end
-
-execute "./composer.phar install -n --no-ansi --prefer-dist" do
-  cwd node["packagist"]["root"]
-  user "vagrant"
-end
--- packagist/recipes/db.rb
+++ packagist/recipes/db.rb
@@ -1,15 +1,3 @@
-template "/tmp/mysql-setup.sql" do
-  source "mysql.erb"
-  mode   "0600"
-  owner  "root"
-  group  "root"
-  variables({
-    :db_name => node["packagist"]["db"]["db_name"]
-  })
-end
-
-execute "mysql -u root < /tmp/mysql-setup.sql"
-
 execute "app/console doctrine:schema:create" do
   cwd            node["packagist"]["root"]
   ignore_failure true  

5fe5efa76bf73d1f4bbd4aaae9240eb37bcaddf1
Thomas Bruederli <bruederli@kolabsys.com>
2013-02-01 20:32:12 +0100

    github oauth required curl

--- php5-ppa/attributes/default.rb
+++ php5-ppa/attributes/default.rb
@@ -2,4 +2,4 @@ default["php5-ppa"]             = {}
 default["php5-ppa"]["ppa"]      = "ondrej"
 default["php5-ppa"]["uri"]      = "http://ppa.launchpad.net/ondrej/php5/ubuntu"
 default["php5-ppa"]["key"]      = "E5267A6C"
-default["php5-ppa"]["packages"] = ["php5-fpm", "php-apc", "php5-xdebug", "php-pear", "php5-intl", "php5-mysqlnd"]
+default["php5-ppa"]["packages"] = ["php5-fpm", "php-apc", "php5-xdebug", "php-pear", "php5-intl", "php5-mysqlnd", "php5-curl"]

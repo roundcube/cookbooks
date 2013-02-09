86a4def49dc54f87ef240599295498d3fffa3991
till <till@php.net>
2013-02-09 22:31:42 +0100

    tomcat-solr recipe from opscode, sans the .war file.

---
+++ tomcat-solr/recipes/default.rb
@@ -0,0 +1,78 @@
+#
+# Cookbook Name:: solr
+# Recipe:: default
+#
+# Copyright 2012, Example Com
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+case node[:platform]
+when "debian", "ubuntu"
+    package "tomcat7" do
+        action :install
+    end
+
+    # configuring tomcat
+    template "/var/lib/tomcat7/conf/Catalina/localhost/solr.xml" do
+        source "tomcat_solr.xml.erb"
+        owner "root"
+        group "tomcat"
+        mode "0664"
+    end
+
+    # creating solr home and solr core
+    directory "#{node[:solr][:home]}/#{node[:solr][:core_name]}" do
+        owner "root"
+        group "tomcat"
+        mode "0777"
+        action :create
+        recursive true
+    end
+
+    directory "#{node[:solr][:home]}/#{node[:solr][:core_name]}/conf" do
+        owner "root"
+        group "tomcat"
+        mode "0777"
+        action :create
+    end
+
+    # configuring solr
+    template "#{node[:solr][:home]}/solr.xml" do
+        source "solr.xml.erb"
+        owner "root"
+        group "root"
+        mode "0664" 
+    end
+
+    template "#{node[:solr][:home]}/#{node[:solr][:core_name]}/conf/solrconfig.xml" do
+        source "solrconfig.xml.erb"
+        owner "root"
+        group "root"
+        mode "0644"
+    end
+
+    template "#{node[:solr][:home]}/#{node[:solr][:core_name]}/conf/schema.xml" do
+        source "schema.xml.erb"
+        owner "root"
+        group "root"
+        mode "0644"
+    end
+
+    #cookbook_file "/var/lib/tomcat7/webapps/solr.war" do
+    #    source "solr.war"
+    #    owner "root"
+    #    group "root"
+    #    mode "0644"
+    #    #      notifies :restart, resources(:service => "tomcat7") 
+    #end
+end

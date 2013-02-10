63ca45dda9b1f91941ccf62f41feaec3792a5f0b
till <till@php.net>
2013-02-10 21:58:03 +0100

    fix permissons

--- packagist/recipes/solr.rb
+++ packagist/recipes/solr.rb
@@ -7,15 +7,22 @@ files.each do |file|
   cookbook_file "#{conf}/#{file}" do
     source file
     owner "root"
-    group "root"
+    group "tomcat7"
     mode "0644"
   end
+end
+
+directory "#{conf}/lang" do
+  mode "0755"
+  owner "root"
+  group "tomcat7"
+  action :create
 end
 
 cookbook_file "#{conf}/lang/stopwords_en.txt" do
   source "stopwords.txt"
   owner "root"
-  group "root"
+  group "tomcat7"
   mode "0644"
 end
 

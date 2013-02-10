ae1dff873808a6750d7fb36003b9de8b866aac73
till <till@php.net>
2013-02-10 22:53:58 +0100

    ownership hammer

--- packagist/recipes/solr.rb
+++ packagist/recipes/solr.rb
@@ -6,7 +6,7 @@ conf  = "/opt/solr/core1/conf"
 files.each do |file|
   cookbook_file "#{conf}/#{file}" do
     source file
-    owner "root"
+    owner "tomcat7"
     group "tomcat7"
     mode "0644"
   end
@@ -14,16 +14,20 @@ end
 
 directory "#{conf}/lang" do
   mode "0755"
-  owner "root"
+  owner "tomcat7"
   group "tomcat7"
   action :create
 end
 
 cookbook_file "#{conf}/lang/stopwords_en.txt" do
   source "stopwords.txt"
-  owner "root"
+  owner "tomcat7"
   group "tomcat7"
   mode "0644"
+end
+
+execute "ensure ownership on tomcat7" do
+  command "chown -R tomcat7:tomcat7 /opt/solr/core1"
 end
 
 service "tomcat7" do

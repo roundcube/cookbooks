a8d7440b2fb2a12098b24bf871ba2748cf0b26f5
Thomas Bruederli <bruederli@kolabsys.com>
2013-04-18 00:02:20 +0200

    Add more cron jobs to keep packagist stats up-to-date

--- packagist/recipes/cron.rb
+++ packagist/recipes/cron.rb
@@ -1,4 +1,4 @@
-commands=["packagist:update --no-debug --env=prod", "packagist:dump --no-debug --env=prod", "packagist:index --no-debug --env=prod"]
+commands=["packagist:update --no-debug --env=prod", "packagist:dump --no-debug --env=prod", "packagist:index --no-debug --env=prod", "packagist:stats:compile --no-debug --env=prod", "packagist:stats --no-debug --env=prod"]
 
 if node.attribute?("live") and node["live"] == true
   app_root = "#{node["packagist"]["live_root"]}/#{node["packagist"]["server_name"]}/current"
